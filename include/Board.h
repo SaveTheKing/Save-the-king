27de09511367fe7cca1767004b2b95d4dca4fd16
moriyaAr <moriyaariel4@gmail.com>
2021-12-27 16:57:09 +0200

    Initial commit

---
+++ include/Board.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "StaticObject.h"
+#include "MovedObject.h"
+#include <vector>
+#include "Helper.h"
+#include "Player.h"
+#include "Drawf.h"
+#include "Level.h"
+using namespace std;
+	
+class Board
+{
+public:
+	void init();
+	void match(int i, int j,char c);
+	void draw();
+	void moveDrawf();
+	Player* getPlayer(int index);
+	int getPlayerSize();
+	Drawf* getDrawf(int index);
+	int getDrawfSize();
+	void setElementArry();
+	bool is_secsseeded();
+	void readLevel();
+	sf::Vector2f next_location(sf::Vector2f direction);
+private:
+	ifstream m_myFile;
+	std::vector <Player*> m_players;
+	std::vector <Drawf*> m_drawf;
+	std::vector<std::vector<StaticObject*>> m_boardElement;
+	sf::Vector2f m_Currlocation, m_nextLocation;
+	std::vector<Location> m_telepotrs; 
+	int m_rowsSize, m_columnsSize;
+	int m_size = 100;
+	Level m_level;
+};
---
+++ src/Drawf.cpp
@@ -0,0 +1,92 @@
+#include "Drawf.h"
+#include "Board.h"
+Drawf::Drawf(int size, int x, int y)
+{
+	init(Graphic::graphic().getPic(25), size, x, y);
+	m_loc.init(x,y);
+}
+
+bool Drawf::condition()
+{
+	return false;
+}
+
+void Drawf::move(Board& m_board)
+{
+	while (true)
+	{
+		switch (m_direction)
+		{
+		case 0:
+		{
+			if (canMove(m_board, MOVE_RATE, 0))
+			{
+				m_pic.move({ MOVE_RATE,0 });
+				return;
+			}
+			else
+				m_direction = (m_direction + 1) % 4;
+			break;
+		}
+		case 1:
+		{
+			if (canMove(m_board, 0, MOVE_RATE))
+			{
+				m_pic.move({ 0,MOVE_RATE });
+				return;
+			}
+			else
+				m_direction = (m_direction+1) % 4;
+			break;
+		}
+		case 2:
+		{
+			if (canMove(m_board, -MOVE_RATE, 0))
+			{
+				m_pic.move({ -MOVE_RATE,0 });
+				return;
+			}
+			else
+				m_direction = (m_direction+1) % 4;
+			break;
+		}
+		case 3:
+		{
+			if (canMove(m_board, 0, -MOVE_RATE))
+			{
+				m_pic.move({ 0,-MOVE_RATE });
+				return;
+			}
+			else
+				m_direction = (m_direction+1) % 4;
+			break;
+		}
+		default:
+			break;
+		}
+	}
+}
+
+bool Drawf::canMove(Board& m_board, double x, double y)
+{
+	sf::Sprite compare = m_pic;
+	compare.move({ (float)x,(float)y });
+	if (((compare.getPosition().x + m_texture.getSize().x) > WIN_X) ||
+		((compare.getPosition().x) < 0 || (compare.getPosition().y) < 0) ||
+		((compare.getPosition().y + m_texture.getSize().y) > WIN_Y))
+		return false;
+	for (int k = 0; k < m_board.getPlayerSize(); k++)
+	{
+		if (compare.getGlobalBounds().intersects(m_board.getPlayer(k)->getSprite().getGlobalBounds()))
+			return false;
+	}
+	for (int k = 0; k < m_board.getDrawfSize(); k++)
+	{
+		if (m_board.getDrawf(k)!=this)
+		{
+			if (m_pic.getGlobalBounds().intersects(m_board.getDrawf(k)->getSprite().getGlobalBounds()))
+				return false;
+		}
+	}
+	return true;
+}
